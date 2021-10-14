ffefb2856c5e70dc7e14ece31b856280ae7075a0
cna-cmd <c-n-a@outlook.com>
2021-10-14 17:46:28 -0300

    modified app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Cards from './components/Cards';
+import City from './components/City';
 import './App.css';
 
 export default function App() {
@@ -9,42 +11,56 @@ export default function App() {
   function onSearch(ciudad) {
     const apiKey = process.env.REACT_APP_KEY_WEATHER;
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-    .then(r => r.json())
-    .then((recurso) => {
-      if(recurso.main !== undefined){
-        const ciudad = {
-          min: Math.round(recurso.main.temp_min),
-          max: Math.round(recurso.main.temp_max),
-          img: recurso.weather[0].icon,
-          id: recurso.id,
-          wind: recurso.wind.speed,
-          temp: recurso.main.temp,
-          name: recurso.name,
-          weather: recurso.weather[0].main,
-          clouds: recurso.clouds.all,
-          latitud: recurso.coord.lat,
-          longitud: recurso.coord.lon
-        };
-        //significa que estas intentando agregar un elemento que ya existe
-        const cityExists = cities.filter(city => city.id === ciudad.id);
-        if(cityExists.length > 0){
-           alert ("The city already exists");
-        }else {
+      .then(r => r.json())
+      .then((recurso) => {
+        if (recurso.main !== undefined) {
+          const ciudad = {
+            min: Math.round(recurso.main.temp_min),
+            max: Math.round(recurso.main.temp_max),
+            img: recurso.weather[0].icon,
+            id: recurso.id,
+            wind: recurso.wind.speed,
+            temp: recurso.main.temp,
+            name: recurso.name,
+            weather: recurso.weather[0].main,
+            clouds: recurso.clouds.all,
+            latitud: recurso.coord.lat,
+            longitud: recurso.coord.lon
+          };
+          const cityExists = cities.filter(city => city.id === ciudad.id);
+          if (cityExists.length > 0) {
+            alert("The city already exists");
+          } else {
             setCities(oldCities => [...oldCities, ciudad]);
-        }     
-      }else {
-        alert("City not found");
-      }
-    });
+          }
+        } else {
+          alert("City not found");
+        }
+      });
   }
   function onClose(id) {
     setCities(oldCities => oldCities.filter(c => c.id !== id));
   }
+  function onFilter(ciudadId) {
+    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
+    if (ciudad.length > 0) {
+      return ciudad[0];
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div className="App">
-      {/* quien debe ejecutar la función onSearch no es el Nav sino el SearchBar */}
-      <Nav onSearch={onSearch}/>
-      <Cards cities={cities} onClose={onClose}/> 
+      <Route path='/'
+        render={() => <Nav onSearch={onSearch} />}
+      />
+      <Route exact path='/'
+        render={() => <Cards cities={cities} onClose={onClose} />}
+      />
+      <Route exact path='/ciudad/:ciudadId'
+        render={({ match }) => <City city={onFilter(match.params.ciudadId)} />}
+      />
     </div>
   );
 }
